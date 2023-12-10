8b86d26253cd928a8b06e8af93a0ae8fd06b3949
Fernando Antônio Ferreira Ibrahim <offut14@gmail.com>
2023-12-09 21:13:27 -0300

    feat: Broker, BolsaDeValores, Correcao UML

--- codigo/BolsaDeValores/src/classes/BolsaDeValores.java
+++ codigo/BolsaDeValores/src/classes/BolsaDeValores.java
@@ -1,5 +1,39 @@
 package classes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BolsaDeValores {
-    
+
+    private static BolsaDeValores bolsaDeValores;
+
+    private Map<String, LivroDeOfertas> acoes;
+
+    private BolsaDeValores(){
+        acoes  = new HashMap<>();
+    }
+
+    public void assinar(String sigla, Broker broker){
+        if(acoes.containsKey(sigla)){
+            
+        }
+    }
+
+    public static BolsaDeValores getInstance() {
+        if (bolsaDeValores == null) {
+            bolsaDeValores = new BolsaDeValores();
+        }
+        return bolsaDeValores;
+    }
+
+    public LivroDeOfertas possuiAcao(String sigla) {
+        LivroDeOfertas livroDeOfertas =  acoes.get(sigla);
+        if(livroDeOfertas == null)
+            //throw new Exception()
+        else
+            return livroDeOfertas;
+    }
+
+
+
 }
--- codigo/BolsaDeValores/src/classes/Broker.java
+++ codigo/BolsaDeValores/src/classes/Broker.java
@@ -31,7 +31,7 @@ public class Broker {
     }
 
     private void upDate(){
-
+        
     }
 
 }
