c0f49ad4139c3f7ba1296d0ae77b3d71f1c7ab4d
Fernando Antônio Ferreira Ibrahim <offut14@gmail.com>
2023-12-13 00:00:54 -0300

    fix test

--- codigo/BolsaDeValores/src/testes/TesteBolsaDeValores.java
+++ codigo/BolsaDeValores/src/testes/TesteBolsaDeValores.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import classes.*;
 
-class BolsaDeValoresTest {
+public class TesteBolsaDeValores {
 
     BolsaDeValores bolsa;
     Acao acao1;
