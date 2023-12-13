23746049b29da487ee0000be7c74430b95b76ff1
Druitti <gabriel.afa@outlook.com>
2023-12-13 18:19:14 -0300

    app atualizado

--- codigo/BolsaDeValores/src/App.java
+++ codigo/BolsaDeValores/src/App.java
@@ -12,68 +12,91 @@ import classes.ordens.*;
 
 public class App {
 
-    
-    private static Lock lock = new ReentrantLock();
-    public static void main(String[] args) throws InterruptedException {
-        LocalDateTime agora = LocalDateTime.now();
+    private static final Lock lock = new ReentrantLock();
+    private static volatile boolean compra = true; // Alternância entre ordens de compra e venda
 
-    
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    public static void main(String[] args) throws InterruptedException {
+
+        BolsaDeValores bolsa = BolsaDeValores.getInstance();
+        Acao acao1 = new Acao("AAPL", "Apple Inc.", "Empresa de tecnologia");
+        Acao acao2 = new Acao("GOOGL", "Alphabet Inc.", "Empresa-mãe do Google");
+        Acao acao3 = new Acao("MSFT", "Microsoft Corporation", "Empresa de tecnologia");
+        Acao acao4 = new Acao("AMZN", "Amazon.com Inc.", "Empresa de comércio eletrônico");
+        Acao acao5 = new Acao("TSLA", "Tesla Inc.", "Fabricante de veículos elétricos");
+        Acao acao6 = new Acao("PETR4", "Petra", "Empresa fabricante de carveja");
+        bolsa.addAcao(acao1);
+        bolsa.addAcao(acao2);
+        bolsa.addAcao(acao3);
+        bolsa.addAcao(acao4);
+        bolsa.addAcao(acao5);
+        bolsa.addAcao(acao6);
+        Broker broker = new Broker(bolsa.getInstance());
+        Broker broker1 = new Broker(bolsa.getInstance());
+        Broker broker2 = new Broker(bolsa.getInstance());
+        Broker broker3 = new Broker(bolsa.getInstance());
+
 
         
-        String dataFormatada = agora.format(formato);
         ExecutorService executor = Executors.newFixedThreadPool(2);
 
-             Runnable ordemCompra = () ->{
+        Runnable ordemCompra = () -> {
+             try {
+                 while (true) {
+                     int quantidade = gerarQuantidadeRandomica(30, 100);
+         double preco = gerarValorRandomico(30.0, 100.0);
 
-                try{
-                    lock.lock();
-                    
-                    BolsaDeValores bolsa = BolsaDeValores.getInstancia();
-                    Broker broker = new Broker(bolsa);
-                    broker.assinar("PETR4");
-                    Acao acao1 = new Acao("10","PETR4","Petrobras");
-                    Ordem ordem = new OrdemCompra(acao1, 100, 26.46, broker);
-                    LivroDeOfertas livroDeOfertas = bolsa.possuiLivroDeOfertas("PETR4");
-                   
-                    System.out.println("Ordem criada: " + ordem);
-                    bolsa.addAcao(acao1);
-                    bolsa.addOperacaoCompraVenda(ordem.toString(), broker);
-                    
-                    System.out.println("Ordem criada: " + ordem);
-                    
+         // Escolhe aleatoriamente entre COMPRA e VENDA
+         OrderType tipoOrdem = (Math.random() < 0.5) ? OrderType.COMPRA : OrderType.VENDA;
+
+         // Escolhe aleatoriamente uma sigla (por exemplo, AAPL)
+         String[] siglas = {"AAPL", "GOOGL", "MSFT", "AMZN", "TSLA"};
+         String sigla = siglas[new Random().nextInt(siglas.length)];
+
+         // Formata a ordem
+         String ordem = String.format("%s;%s;%d;%.2f;BKR", tipoOrdem, sigla, quantidade, preco);    
+        // Adiciona a ordem à bolsa
+        broker.enviarOrdem(ordem);
                     Thread.sleep(1000);
-                    lock.unlock();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-
-                }
-             }
-
-                Runnable ordemVenda = () ->{
-                    try{
-                        lock.lock();
-                        BolsaDeValores bolsa = BolsaDeValores.getInstancia();
-                        bolsa.adicionarOrdem(ordem);
                     
-                        System.out.println("Ordem criada: " + ordem);
-                        Thread.sleep(1000);
-                        lock.unlock();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-    
-                    }
-                }
-          
-            Thread tOrdemCompra = new Thread(ordemCompra);
-            Thread tOrdemVenda = new Thread(ordemVenda);
-            executor.submit(tOrdemVenda);
-            executor.submit(tOrdemCompra);
-
-    
                 
             
-        }
+        }}
+         catch (InterruptedException e) {
+                e.printStackTrace();
+            }};}
 
-        
+    /*
+     * Runnable ordemVenda = () -> {
+     * try {
+     * while (true) {
+     * lock.lock();
+     * if (!compra) {
+     * criarOrdem("Ordem de Venda");
+     * compra = true; // Troca para próxima thread
+     * }
+     * lock.unlock();
+     * Thread.sleep(1000);
+     * }
+     * } catch (InterruptedException e) {
+     * e.printStackTrace();
+     * }
+     * };
+     * 
+     * Thread tOrdemCompra = new Thread(ordemCompra);
+     * Thread tOrdemVenda = new Thread(ordemVenda);
+     * 
+     * tOrdemCompra.start();
+     * tOrdemVenda.start();
+     * 
+     * // executor.shutdown(); // Não é necessário para threads infinitas
+     * }
+     */
+    private static int gerarQuantidadeRandomica(int min, int max) {
+        return new Random().nextInt(max - min + 1) + min;
+    }
+
+    private static double gerarValorRandomico(double min, double max) {
+        return new Random().nextDouble() * (max - min) + min;
+    }
+
 }
