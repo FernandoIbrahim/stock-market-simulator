a729bdde95e54f3f8d46c099e4afb670069c7ef2
Druitti <gabriel.afa@outlook.com>
2023-12-13 23:47:57 -0300

    Documentação Completa

--- codigo/BolsaDeValores/src/classes/TransacaoManager.java
+++ codigo/BolsaDeValores/src/classes/TransacaoManager.java
@@ -1,3 +1,7 @@
+/**
+ * Classe responsável por gerenciar as transações entre as ordens de compra e venda
+ */
+
 package classes;
 
 import java.time.LocalDateTime;
@@ -8,7 +12,13 @@ import classes.ordens.*;;
 
 
 public class TransacaoManager {
-
+    /**
+     *   Metodo que cria uma transação caso seja possível
+     * @param ordemCompra recebe uma ordem de compra
+     * @param ordemVenda recebe uma ordem de venda
+     * @param livro recebe um livro de ofertas
+     * @return retorna uma transação
+     */
     public static synchronized Transacao criarTransacao(OrdemConcrets ordemCompra, OrdemConcrets ordemVenda, LivroDeOfertas livro) {
         if (podeCriarTransacao(ordemCompra, ordemVenda)) {
             int quantidadeTransacao = Math.min(ordemCompra.getQuantidade(), ordemVenda.getQuantidade());
@@ -26,13 +36,24 @@ public class TransacaoManager {
         }
         return null;
     }
-
+    /**
+     * Metodo que verifica se é possível criar uma transação
+     * @param ordemCompra recebe uma ordem de compra
+     * @param ordemVenda recebe uma ordem de venda
+     * @return retorna um true se é possível criar uma transação false se não
+     */
     private static synchronized boolean podeCriarTransacao(OrdemConcrets ordemCompra, OrdemConcrets ordemVenda) {
         return ordemCompra.getValor() >= ordemVenda.getValor();
     }
 
  
-
+    /**
+     * Metodo que atualiza as ordens de compra e venda
+     * @param ordemCompra recebe uma ordem de compra
+     * @param ordemVenda recebe uma ordem de venda 
+     * @param quantidadeTransacao recebe uma quantidade de compra que foi feita de uma açao
+     * @return atualiza as ordens de compra e venda 
+     */
     private static synchronized void  atualizarOrdens(OrdemConcrets ordemCompra, OrdemConcrets ordemVenda, int quantidadeTransacao) {
         int comparacao = ordemCompra.compareTo(ordemVenda);
 
